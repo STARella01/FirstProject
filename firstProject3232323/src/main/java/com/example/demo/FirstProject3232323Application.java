f567790d9013bcfb6656fc75ac3e15777b5c3d09
eongdeong-ihaejeogdan <eongdeong-ihaejeogdan@localhost>
2023-11-01 16:48:09 +0900

    ddddd

---
+++ firstProject3232323/src/main/java/com/example/demo/FirstProject3232323Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProject3232323Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProject3232323Application.class, args);
+	}
+
+}
