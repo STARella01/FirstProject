9d229fde6ea00d2667af68a7a2c694ed0892ca09
tjoeun <tjoeun@host.docker.internal>
2023-11-02 16:49:17 +0900

    231102 강한별 - 깃 업로드 테스트

--- muinproject/src/main/java/com/example/demo/MuinprojectApplication.java
+++ muinproject/src/main/java/com/example/demo/MuinprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MuinprojectApplication {
 
 	public static void main(String[] args) {
+		// git 올리는 것 테스트
 		SpringApplication.run(MuinprojectApplication.class, args);
 	}
 
